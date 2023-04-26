c9ea6217969f0eb785ecb4e6226cc33399ab172b
Belster24 <brossjefrajer@gmail.com>
2023-04-26 17:16:33 +0200

    other changes

--- Assets/Scripts/CircleInstantiator.cs
+++ Assets/Scripts/CircleInstantiator.cs
@@ -1,11 +1,20 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CircleInstantiator : MonoBehaviour
 {
-    [SerializeField]int numObjects = 12;
+    [SerializeField]int numObjects = 24;
     [SerializeField] float radius;
+    [SerializeField] float dotSpeed = 10f;
+    [SerializeField] float speedDecrease = 1f;
+
     public GameObject prefab;
     AudioSource audioSource;
+    List<EchoLocation> dots = new List<EchoLocation>();
+    EchoLocation previousDotListed = null;
+
+
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -21,14 +30,36 @@ public class CircleInstantiator : MonoBehaviour
     {
         audioSource.Play();
         Vector3 center = transform.position;
+
+        previousDotListed = null;
         for (int i = 0; i < numObjects; i++)
+            dots.Add(null); // fill list with nulls
+        
+        for (int i = 0; i <= numObjects; i++)
         {
             int a = i * 30;
             Vector3 pos = RandomCircle(center, radius, a);
             GameObject obj = Instantiate(prefab, pos, Quaternion.identity);
+            
+            dots.Add(obj.GetComponent<EchoLocation>());
+            obj.GetComponent<EchoLocation>().ID = i;
+            if (previousDotListed != null)
+            {
+                previousDotListed.nextDot = obj.GetComponent<EchoLocation>();
+                obj.GetComponent<EchoLocation>().previousDot = previousDotListed;
+
+            }
+            previousDotListed = obj.GetComponent<EchoLocation>();
+            if (i == numObjects)
+                obj.GetComponent<EchoLocation>().nextDot = dots[0];
+            
             Vector2 dir = transform.position - pos;
+
+            obj.GetComponent<EchoLocation>().speed = dotSpeed;
+
             obj.transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
         }
+
     }
     Vector3 RandomCircle(Vector3 center, float radius, int a)
     {
--- Assets/Scripts/EchoLocation.cs
+++ Assets/Scripts/EchoLocation.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 public class EchoLocation : MonoBehaviour
 {
-    Rigidbody2D rigidbody;  
+    public int ID;
+    Rigidbody2D rigidbody;
+    public LineRenderer line;
+    private bool hasLine = true;
     [SerializeField] float forceMagnitude;
     [SerializeField] float shrinkRate;
+    [SerializeField] public float speed;
+    [SerializeField] float speedDecrease;
+    public float newThickness;
+    public EchoLocation previousDot;
+    public EchoLocation nextDot;
+
     int collisionCount;
     private void Start()
     {
   
+        line = GetComponent<LineRenderer>();
+        
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.AddForce(-transform.up * forceMagnitude, ForceMode2D.Impulse);
+        rigidbody.AddForce(-transform.up * speed, ForceMode2D.Impulse);
 
     }
 
@@ -22,6 +35,16 @@ public class EchoLocation : MonoBehaviour
         Vector3 newScale = transform.localScale;
         newScale -= Vector3.one * shrinkRate * Time.deltaTime;
         transform.localScale = newScale;
+        
+        if (previousDot != null && hasLine)
+        {
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, previousDot.transform.position);
+            newThickness = newScale.x;
+            line.SetWidth(newThickness,newThickness);
+        }
+        
+        
         if (newScale.x <= 0f)
         {
             Destroy(gameObject);
@@ -32,18 +55,23 @@ public class EchoLocation : MonoBehaviour
 
         if (collision.gameObject.tag == "wall")
         {
-            gameObject.GetComponent<SpriteRenderer>().color = collision.gameObject.GetComponent<Light2D>().color;
+            gameObject.GetComponentInChildren<SpriteRenderer>().color = collision.gameObject.GetComponent<Light2D>().color;
+            line.startColor = collision.gameObject.GetComponent<Light2D>().color;
+            nextDot!.line.endColor = collision.gameObject.GetComponent<Light2D>().color;
             collision.gameObject.GetComponent<LightFunction>().enableLight();
 
         } else if (collision.gameObject.tag == "enemy")
         {
-
-            gameObject.GetComponent<SpriteRenderer>().color = collision.gameObject.GetComponent<Light2D>().color;
+            gameObject.GetComponentInChildren<SpriteRenderer>().color = collision.gameObject.GetComponent<Light2D>().color;
+            line.startColor = collision.gameObject.GetComponent<Light2D>().color;
+            nextDot!.line.endColor = collision.gameObject.GetComponent<Light2D>().color;
             collision.gameObject.GetComponent<LightFunction>().enableLight();
-        }else if (collision.gameObject.tag == "lock")
+        } else if (collision.gameObject.tag == "lock")
         {
-
-            gameObject.GetComponent<SpriteRenderer>().color = collision.gameObject.GetComponent<Light2D>().color;
+            gameObject.GetComponentInChildren<SpriteRenderer>().color = collision.gameObject.GetComponent<Light2D>().color;
+            line.startColor = collision.gameObject.GetComponent<Light2D>().color;
+            nextDot!.line.endColor = collision.gameObject.GetComponent<Light2D>().color;
+            rigidbody.AddForce(-transform.up * speed, ForceMode2D.Impulse);
             collision.gameObject.GetComponent<LightFunction>().enableLight();
         }
     }
