6dd10f331c16e52e96bce8930aea4c4868da9fe7
Malik Nouman <noumanali.ali.0@gmail.com>
2023-06-05 18:44:05 +0200

    ss

--- Assets/Scripts/TweenBackAndForth.cs
+++ Assets/Scripts/TweenBackAndForth.cs
@@ -8,7 +8,8 @@ public class TweenBackAndForth : MonoBehaviour
     public Vector2 moveDistance = new Vector2(1f, 1f); // Distance to move in each axis
     public float tweenDuration = 1f; // Duration of each tween
     public Ease easeType = Ease.Linear; // Type of easing to use
-
+    public int numLoops = -1; // Number of loops (-1 for infinite loops)
+    public LoopType loopType = LoopType.Yoyo; // Type of loop
     private Vector3 initialPosition;
 
     private void Start()
@@ -20,27 +21,15 @@ public class TweenBackAndForth : MonoBehaviour
     private void StartTween()
     {
         // Tween the object back and forth in the x axis
-        transform.DOMoveX(initialPosition.x + moveDistance.x, tweenDuration)
+         transform.DOMoveX(initialPosition.x + moveDistance.x, tweenDuration)
             .SetEase(easeType)
-            .OnComplete(() => ReverseTweenX());
+            .SetLoops(numLoops, loopType);
 
         // Tween the object back and forth in the y axis
         transform.DOMoveY(initialPosition.y + moveDistance.y, tweenDuration)
             .SetEase(easeType)
-            .OnComplete(() => ReverseTweenY());
+            .SetLoops(numLoops, loopType);
     }
 
-    private void ReverseTweenX()
-    {
-        transform.DOMoveX(initialPosition.x - moveDistance.x, tweenDuration)
-            .SetEase(easeType)
-            .OnComplete(() => StartTween());
-    }
-
-    private void ReverseTweenY()
-    {
-        transform.DOMoveY(initialPosition.y - moveDistance.y, tweenDuration)
-            .SetEase(easeType)
-            .OnComplete(() => StartTween());
-    }
+  
 }
