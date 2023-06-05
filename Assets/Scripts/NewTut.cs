1e7d79b7a3830ba97863cb7988d9c440b1e5f5b4
Malik Nouman <noumanali.ali.0@gmail.com>
2023-06-05 21:44:40 +0200

    its over

--- Assets/Scripts/NewTut.cs
+++ Assets/Scripts/NewTut.cs
@@ -5,14 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class NewTut : MonoBehaviour
 {
-
+    [SerializeField] BellAndDoor bd;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "door")
         {
+            
             PlayerPrefs.SetInt("tutorial", 1);
             SceneManager.LoadScene("level_1");
         }
-
+        if (collision.gameObject.tag == "lock")
+        {
+            Destroy(collision.gameObject);
+            bd.count++;
+        }
     }
 }
